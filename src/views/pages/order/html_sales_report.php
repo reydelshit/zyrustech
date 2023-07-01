74bee2dcade5d0a55431ab45b19e35c88e198cba
Reydel Ocon <reydel321@gmail.com>
2023-07-01 11:50:21 +0800

    lmao

--- src/views/pages/order/html_sales_report.php
+++ src/views/pages/order/html_sales_report.php
@@ -1,24 +1,24 @@
 <?php
 
-// Create a database connection
+
 $conn = mysqli_connect(DATABASE_SERVER, DATABASE_USER_NAME, DATABASE_PASSWORD, DATABASE_NAME, DATABASE_PORT);
 
-// Check if the connection was successful
+
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
 
-// Check if a filter is set
+
 $filter = isset($_GET['filter']) ? $_GET['filter'] : '';
 
-// Prepare the SQL query to fetch the sales report from the orders table based on the filter
-$sql = "SELECT o.id, SUM(o.amount) as total_amount, o.type, o.payment_method, p.name, c.name 
+
+$sql = "SELECT o.id, o.amount, o.type, o.payment_method, GROUP_CONCAT(DISTINCT p.name SEPARATOR ', ') AS product_names, GROUP_CONCAT(DISTINCT c.name SEPARATOR ', ') AS category_names
         FROM orders AS o
         INNER JOIN order_product AS op ON o.id = op.order_id
         INNER JOIN product AS p ON op.product_id = p.id
         INNER JOIN category AS c ON p.category_id = c.id";
 
-// Add the filter conditions to the SQL query if a filter is set
+
 if ($filter === 'day') {
     $sql .= " WHERE DATE(o.created_at) = CURDATE()";
 } elseif ($filter === 'week') {
@@ -29,24 +29,24 @@ if ($filter === 'day') {
 
 $sql .= " GROUP BY o.id";
 
-// Execute the query
+
 $result = mysqli_query($conn, $sql);
 
-// Check if the query execution was successful
+
 if ($result) {
     // Generate the sales report
     $sales_report = array();
-    $sales_total = 0; // Variable to calculate the sales total
+    $sales_total = 0;
 
-    // Loop through the result set
+
     while ($row = mysqli_fetch_assoc($result)) {
         $order = array(
             'id' => $row['id'],
-            'amount' => $row['total_amount'],
+            'amount' => $row['amount'],
             'type' => $row['type'],
             'payment_method' => $row['payment_method'],
-            'product_name' => $row['name'],
-            'name' => $row['name'] // Include category name
+            'product_names' => $row['product_names'],
+            'category_names' => $row['category_names']
         );
 
         // Add the order to the sales report
@@ -56,8 +56,7 @@ if ($result) {
         $sales_total += $order['amount'];
     }
 
-    // Close the database connection
-    mysqli_close($conn);
+
 
     // Display the sales report
     echo "<h2>Sales Report</h2>";
@@ -77,10 +76,9 @@ if ($result) {
     echo "<thead>";
     echo "<tr>";
     echo "<th style='border: 1px solid #000; padding: 8px;'>ID</th>";
-    echo "<th style='border: 1px solid #000; padding: 8px;'>Type</th>";
     echo "<th style='border: 1px solid #000; padding: 8px;'>Payment Method</th>";
     echo "<th style='border: 1px solid #000; padding: 8px;'>Product</th>";
-    echo "<th style='border: 1px solid #000; padding: 8px;'>Category</th>"; // Category column
+    echo "<th style='border: 1px solid #000; padding: 8px;'>Category</th>";
     echo "<th style='border: 1px solid #000; padding: 8px;'>Amount</th>";
     echo "</tr>";
     echo "</thead>";
@@ -89,18 +87,19 @@ if ($result) {
     foreach ($sales_report as $order) {
         echo "<tr>";
         echo "<td style='border: 1px solid #000; padding: 8px;'>" . $order['id'] . "</td>";
-        echo "<td style='border: 1px solid #000; padding: 8px;'>" . $order['type'] . "</td>";
         echo "<td style='border: 1px solid #000; padding: 8px;'>" . $order['payment_method'] . "</td>";
-        echo "<td style='border: 1px solid #000; padding: 8px;'>" . $order['product_name'] . "</td>";
-        echo "<td style='border: 1px solid #000; padding: 8px;'>" . $order['name'] . "</td>"; // Display category
+        echo "<td style='border: 1px solid #000; padding: 8px;'>" . $order['product_names'] . "</td>";
+        echo "<td style='border: 1px solid #000; padding: 8px;'>" . $order['category_names'] . "</td>";
         echo "<td style='border: 1px solid #000; padding: 8px;'>" . $order['amount'] . "</td>";
         echo "</tr>";
     }
 
+
+
     // Display the total sales row
     echo "<tr>";
-    echo "<td colspan='5' style='text-align: right; border: 1px solid #000; padding: 8px;'><strong>Total Sales:</strong></td>";
-    echo "<td style='border: 1px solid #000; padding: 8px;'>" . $sales_total . "</td>";
+    echo "<td colspan='4' style='text-align: right; border: 1px solid #000; padding: 8px;'><strong>Total Sales:</strong></td>";
+    echo "<td style='border: 1px solid #000; padding: 8px;'>₱ " . number_format($sales_total) . "</td>";
     echo "</tr>";
 
     echo "</tbody>";
@@ -108,28 +107,32 @@ if ($result) {
 
     echo "<br /><br /><br /><br /><br /><br /><br />";
 
+
+
     if (isset($_GET['generate_report'])) {
+
         // Your existing code to generate the sales report
+        $sql = "SELECT SUM(p.price) AS total_amount, c.name AS category_name
+        FROM order_product AS op
+        INNER JOIN product AS p ON op.product_id = p.id
+        INNER JOIN category AS c ON p.category_id = c.id
+        GROUP BY c.id, c.name";
+
+        // Execute the SQL query and fetch the sales report
+        $result = mysqli_query($conn, $sql);
+        $sales_report = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
         // Prepare the chart data
         $categories = array();
         $salesAmounts = array();
 
         foreach ($sales_report as $order) {
-            $category = $order['name']; // Use category name
-            $amount = $order['amount'];
+            $category = $order['category_name']; // Use category name
+            $amount = $order['total_amount'];
 
-            // If the category already exists in the array, add the amount to its corresponding index
-            if (array_key_exists($category, $categories)) {
-                $salesAmounts[$category] += $amount;
-            }
-            // Otherwise, create a new entry in both arrays
-            else {
-                $categories[$category] = $category;
-                $salesAmounts[$category] = $amount;
-            }
+            $categories[] = $category;
+            $salesAmounts[] = $amount;
         }
-
 
         // Generate the bar graph
         echo "<h2>Sales by Category (Bar Graph)</h2>";
@@ -142,10 +145,10 @@ if ($result) {
         echo "var barChart = new Chart(ctx, {";
         echo "type: 'bar',";
         echo "data: {";
-        echo "labels: " . json_encode(array_values($categories)) . ",";
+        echo "labels: " . json_encode($categories) . ",";
         echo "datasets: [{";
         echo "label: 'Sales',";
-        echo "data: " . json_encode(array_values($salesAmounts)) . ",";
+        echo "data: " . json_encode($salesAmounts) . ",";
         echo "backgroundColor: 'rgba(255, 165, 0, 0.2)',"; // Orange color
         echo "borderColor: 'rgba(255, 165, 0, 1)',"; // Orange color
         echo "borderWidth: 1";
@@ -166,6 +169,9 @@ if ($result) {
         echo "<button type='submit' name='generate_report' value='true' >Generate Report</button>";
         echo "</form>";
     }
+
+    // Close the database connection
+    mysqli_close($conn);
 } else {
     // Display an error message if the query execution failed
     echo "Error: " . mysqli_error($conn);
